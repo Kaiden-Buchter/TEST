3054de21366414fd50d8b1ade37527a25545712b
Kaiden Buchter <150194619+Kaiden-Buchter@users.noreply.github.com>
2024-03-07 18:49:27 +0000

    Add canvas element and display it on button click

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let button = document.getElementById('start');
 let body = document.querySelector('body');
+let canvas = document.querySelector('canvas');
 
 button.addEventListener('click', function() {
   button.style.display = 'none';
+  canvas.style.display = 'block';
   body.style.backgroundColor = 'lightblue';
 });
+
